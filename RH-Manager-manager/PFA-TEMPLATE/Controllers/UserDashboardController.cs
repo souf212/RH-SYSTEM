9b214d20cff825480f2936a2916b2e5496aa4ed1
soufiane <souf2001elotmani@gmail.com>
2025-03-28 03:14:48 +0000

    createuser->manager->employes

---
+++ RH-Manager-manager/PFA-TEMPLATE/Controllers/UserDashboardController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PFA_TEMPLATE.Controllers
+{
+    [Authorize(Roles = "Employes")] // Restrict access to standard users
+    public class UserDashboardController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "User Dashboard";
+             
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            ViewData["Title"] = "User Profile";
+            // Add logic to fetch and display user profile
+            return View();
+        }
+
+        public IActionResult Tasks()
+        {
+            ViewData["Title"] = "My Tasks";
+            // Add logic to fetch and display user tasks
+            return View();
+        }
+    }
+}
--- RH-Manager/PFA-TEMPLATE/ViewModels/VonageSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PFA_TEMPLATE.ViewModels
-{
-    public class VonageSettings
-    {
-        public string ApiKey { get; set; }
-        public string ApiSecret { get; set; }
-    } 
-}
