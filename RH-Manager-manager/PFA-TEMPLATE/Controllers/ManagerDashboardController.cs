1071aed71335bc98fa3e486ededf167b7ab0451e
soufiane <souf2001elotmani@gmail.com>
2025-03-29 02:37:14 +0000

    user->manager->employes

---
+++ RH-Manager-manager/PFA-TEMPLATE/Controllers/ManagerDashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PFA_TEMPLATE
+{ 
+
+    [Authorize(Roles = "Manager")]
+    public class ManagerDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
